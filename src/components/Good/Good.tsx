e7bf3e93dec8e834f2a0d7be65c9735c40829727
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-02-26 22:02:49 +0300

    add GUI

--- src/components/Good/Good.tsx
+++ src/components/Good/Good.tsx
@@ -1,8 +1,19 @@
 import { FC } from "react";
-import styles from './GoodsList.module.css';
+import styles from './Good.module.css';
 
-const Good: FC = () => {
-  return <div className={styles.goods}>Goods</div>
+interface IGood {
+  item: any;
+}
+
+const Good: FC<IGood> = ({ item }) => {
+  return (
+    <li key={item.id} className={styles.good}>
+      <p>{item.product}</p>
+      <p>{item.brand}</p>
+      <p>{item.price}</p>
+      <p>id: {item.id}</p>
+    </li>
+  )
 };
 
 export default Good;
--- src/components/GoodsList/GoodsList.tsx
+++ src/components/GoodsList/GoodsList.tsx
@@ -1,28 +1,73 @@
-import { FC, useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import styles from './GoodsList.module.css';
 import { getGoods } from "../api/api";
+import Good from "../Good/Good";
 
 const GoodsList: FC = () => {
   const [goodsList, setGoodsList] = useState<any>([]); // типизировать
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number | null>(null);
+  const [searchString, setSearchString] = useState<string>('');
+  const [filteredGoods, setFilteredGoods] = useState<any>([]); // типизировать
+
   // подсчёт количества страниц в зависимости от количества элементов
   const pages = Math.ceil(goodsList.length / 50);
 
   const getDataFromServer = async () => {
     const products = await getGoods();
     setGoodsList(products.flat());
+    setFilteredGoods(products.flat());
     setTotalPages(pages);
   }
   console.log(goodsList);
   console.log(totalPages);
-
+// получение данных при монтировании компонента
   useEffect(() => {
     getDataFromServer();
   }, [])
+// функция, фильтрующая товары на основе строки в инпуте
+  const filterGoods = (str: string) => { // типизация
+    const filtered = goodsList.filter((item: any) =>
+      item.product.toLowerCase.includes(str.toLowerCase()) ||
+      item.id.toLowerCase.includes(str.toLowerCase()) ||
+      (item.brand && item.brand.toLowerCase.includes(str.toLowerCase())) ||
+      (item.price && item.price.toString().toLowerCase().includes(str.toLowerCase()))
+    )
+    setFilteredGoods(filtered);
+    setTotalPages(Math.ceil(filtered.length / 50));
+    setPage(1);
+  };
+// хэндлер для фильтрации
+  const handleFilterGoods = (evt: ChangeEvent<HTMLInputElement>) => {
+    setSearchString(evt.target.value);
+    filterGoods(evt.target.value);
+  };
+  // функция пагинации
+  const setPagination = (array: any, length: number, pageNumber: number) => {
+    return array.slice((pageNumber - 1) * length, pageNumber * length);
+  }
 
-
-  return <div className={styles.goods}>Goods</div>
+  return (
+    <div className={styles.goods}>
+      <input
+        type="text"
+        name="goodsFilter"
+        placeholder="Поиск товара"
+        value={searchString}
+        onChange={handleFilterGoods}
+        className={styles.goods__input}
+      />
+      <ul className={styles.goods__list}>
+      {setPagination(filteredGoods, 50, page).map((item: any) => (
+        <Good 
+          item={item}
+        />
+      ))}
+      </ul>
+    </div>
+  )
 };
 
 export default GoodsList;
