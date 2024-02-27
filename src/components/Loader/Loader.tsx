22bde2a5681065f9695a1cd17357ce9a81f42649
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-02-27 10:28:01 +0300

    fix loader style

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -6,7 +6,11 @@ interface ILoader {
 }
 
 const Loader: FC<ILoader> = ({ text }) => {
-  return <div className={styles.loader}>{text}</div>;
+  return (
+    <div className={styles.loader}>
+      <h3 className={styles.title}>{text}</h3>
+    </div>
+  );
 }
 
 export default Loader;
